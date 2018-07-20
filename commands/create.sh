480d6e0cf07b2aac437ebfeb9a93a20569614b0d
William Correa <wilcorrea@gmail.com>
2018-07-20 12:04:52 -0300

    Samples engine

--- commands/create.sh
+++ commands/create.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-BASE=${1}/domains
+BASE=${1}
 DOMAIN=${2}
 SAMPLE=${3}
-REPO=${BASE}/${DOMAIN}/repo
-APP=${BASE}/${DOMAIN}/app
+
+SAMPLES=${BASE}/samples
+DOMAINS=${BASE}/domains
+
+REPO=${DOMAINS}/${DOMAIN}/repo
+APP=${DOMAINS}/${DOMAIN}/app
 
 if [[ ! "${SAMPLE}" ]];then
   SAMPLE="php"
@@ -14,12 +18,12 @@ fi
 mkdir -p ${REPO}
 cd ${REPO}
 git init --bare
-cp -TRv ${BASE}/samples/${SAMPLE}/repo/ ${REPO}/hooks/
+cp -TRv ${SAMPLES}/${SAMPLE}/repo/ ${REPO}/hooks/
 find ${REPO}/hooks -type f -exec sed -i "s/{domain}/${DOMAIN}/g" {} \;
 chmod +x ${REPO}/hooks/post-receive
 
 # APP
-cp -TRv ${BASE}/samples/${SAMPLE}/ ${APP}/
+cp -TRv ${SAMPLES}/${SAMPLE}/app/ ${APP}/
 cd ${APP}
 find ${APP} -type f -exec sed -i "s/{domain}/${DOMAIN}/g" {} \;
 docker-compose up -d
