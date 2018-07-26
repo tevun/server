e2b6a6eda9500e326af17655e3588a96e9b1eab7
William Correa <wilcorrea@gmail.com>
2018-07-26 09:30:45 -0300

    Fix

--- commands/setup.sh
+++ commands/setup.sh
@@ -14,7 +14,7 @@ if [ ! -d "${BASE}/domains}" ];then
 fi
 chmod 755 ${BASE}/domains
 chown ${TEVUN_USER}:docker ${BASE}/domains
-if [ ! -L "/domains}" ];then
+if [ ! -h "/domains}" ];then
   ln -s ${BASE}/domains /domains
 fi
 
