7a20d4f6e1d365e509d081f76a8890ca63d1eab8
William Correa <wilcorrea@gmail.com>
2019-04-04 16:15:38 -0300

    Create compose.sh

---
+++ commands/utils/compose.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# https://gist.github.com/deviantony/2b5078fe1675a5fedabf1de3d1f2652a
+
+# get latest docker compose released tag
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+# Install docker-compose
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod +x /usr/local/bin/docker-compose
+sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+# Output compose version
+docker-compose -v
