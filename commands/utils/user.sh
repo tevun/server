e275337434035dc447a5ef917f95fc715a7d8468
William Correa <wilcorrea@gmail.com>
2019-04-04 16:02:29 -0300

    Update user.sh

--- commands/utils/user.sh
+++ commands/utils/user.sh
@@ -2,25 +2,12 @@
 
 INSTALL_USER=${2}
 
-echo "[1/3] ~> Create '${INSTALL_USER}'"
+echo "[1/2] ~> Create '${INSTALL_USER}'"
 EXISTS=$(grep -c ^${INSTALL_USER}: /etc/passwd)
 if [ "$EXISTS" = '0' ]; then
   useradd -u 1000 ${INSTALL_USER}
-  mkdir -p /home/${INSTALL_USER}/.ssh/
-  cp ~/.ssh/authorized_keys /home/${INSTALL_USER}/.ssh/
-  chmod 755 /home/${INSTALL_USER}/.ssh/authorized_keys
-  chown -R ${INSTALL_USER}:${INSTALL_USER} /home/${INSTALL_USER}
-  echo "deploy ALL=(ALL) ALL" > /etc/sudoers.d/deploy
 fi
 
-SSH=$(grep -c ^AllowUsers.*${INSTALL_USER}: /etc/ssh/sshd_config)
-if [ "$EXISTS" = '0' ]; then
-  echo "[2/3] ~> Configure ssh"
-  printf '\n%s %s\n' 'AllowUsers' ${INSTALL_USER} >> /etc/ssh/sshd_config
-else 
-  echo "[2/3] ~> Service ssh can not be modified"
-fi
-
-echo "[3/3] ~> Add '${INSTALL_USER}' to docker and root groups"
+echo "[2/2] ~> Add '${INSTALL_USER}' to docker and root groups"
 usermod -aG docker ${INSTALL_USER}
 usermod -aG root ${INSTALL_USER}
