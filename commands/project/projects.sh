29f7560e90626cb0f1985a2057368ecf1b7cd318
William Correa <wilcorrea@gmail.com>
2019-05-22 13:50:04 -0300

    [#2/doc] Add instruction to mysql connection via bash > current branch: master, issue link: #> current branch: master commands/project/projects.sh

--- commands/project/projects.sh
+++ commands/project/projects.sh
@@ -7,3 +7,14 @@ PROJECTS=${BASE}/projects
 
 ls -lah ${PROJECTS} --color | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) \
              *2^(8-i));if(k)printf("%0o ",k);print}'
+
+#TEVUN_RESULTS=($(mysql -Bse "SELECT a, b, c, d FROM projects;"))
+#TEVUN_RESULTS_TOTAL=${#TEVUN_RESULTS[@]}
+#for (( i=0 ; i<${TEVUN_RESULTS_TOTAL} ; i++ ))
+#do
+#    echo "Record No. $i: ${TEVUN_RESULTS[$i]}"
+#    fieldA=${TEVUN_RESULTS[0]};
+#    fieldB=${TEVUN_RESULTS[1]};
+#    fieldC=${TEVUN_RESULTS[2]};
+#    fieldD=${TEVUN_RESULTS[3]};
+#done
