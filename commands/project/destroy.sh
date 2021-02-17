4bbc27cae0ce74d9b7637ada30c5c115f185c9b2
William Correa <wilcorrea@gmail.com>
2021-02-17 00:19:35 -0300

    [feature] Improve templates (create laravel and improve php and wordpress)

--- commands/project/destroy.sh
+++ commands/project/destroy.sh
@@ -12,11 +12,11 @@ if [[ ! -d ${PROJECTS}/${PROJECT} ]]; then
   exit 0
 fi
 
-__plot "[1/1] Destroy '${PROJECT}' (${PROJECTS}/${PROJECT})? y/[N]: "
+__plot "[1/1] Destroy '${PROJECT}' (${PROJECTS}/${PROJECT})? yes/[N]: "
 read -r DESTROY
 
 TEVUN_MESSAGE="The project '${PROJECT}' was not destroyed"
-if [[ ${DESTROY} = "y" ]]; then
+if [[ ${DESTROY} = "yes" ]]; then
   cd "${APP}" || exit
   if [[ -f "docker-compose.yml" ]]; then
     docker-compose down --volumes
