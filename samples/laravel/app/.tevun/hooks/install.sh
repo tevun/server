440da4b04c2add26c790c86bcaaa73e006d4c476
William Correa <wilcorrea@gmail.com>
2021-02-16 22:38:50 -0300

    [feature] Improve templates (create laravel and improve php and wordpress)

--- samples/laravel/app/.tevun/hooks/install.sh
+++ samples/laravel/app/.tevun/hooks/install.sh
@@ -4,5 +4,5 @@ echo " ~> [hooks\install.sh] on [${1}, ${2}]"
 
 cd "${1}" || exit
 
-docker exec "{alias}-nginx" bash -c "su -c \"composer install --no-interaction -optimize-autoloader\" application"
+docker exec "{alias}-nginx" bash -c "su -c \"composer install --no-interaction --optimize-autoloader\" application"
 docker exec "{alias}-nginx" bash -c "su -c \"php artisan migrate --force\" application"
